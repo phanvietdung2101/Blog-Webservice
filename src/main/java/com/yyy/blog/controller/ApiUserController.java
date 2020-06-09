1b68f458fbb65be348c2d2f6d9d63d94ded4ac59
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-06-09 23:18:30 +0700

    fix api controller better

--- src/main/java/com/yyy/blog/controller/ApiUserController.java
+++ src/main/java/com/yyy/blog/controller/ApiUserController.java
@@ -4,9 +4,12 @@ import com.yyy.blog.model.User;
 import com.yyy.blog.service.UserService;
 import com.yyy.blog.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class ApiUserController {
@@ -16,11 +19,12 @@ public class ApiUserController {
     @PostMapping("/api/user/create-user")
     public ResponseEntity<Void> createUser(@RequestBody User user){
         userService.addNewUser(user);
-        return new ResponseEntity<Void>();
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/api/user/{id}")
-    public User getUser(@PathVariable long id){
-        return this.userService.findUserById(id);
+    public ResponseEntity<User> getUser(@PathVariable long id){
+        Optional<User> optionalUser = userService.findUserById(id);
+        return optionalUser.map(user -> new ResponseEntity<>(user, HttpStatus.FOUND)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
--- src/main/java/com/yyy/blog/service/impl/UserServiceImpl.java
+++ src/main/java/com/yyy/blog/service/impl/UserServiceImpl.java
@@ -26,25 +26,25 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Optional<User> findUserById(long id) {
-        return this.userRepository.findById(id);
+        return userRepository.findById(id);
     }
 
     @Override
     public void addNewUser(User user) {
-        Optional<Role> optionalRole = this.roleRepository.findRoleByName("ROLE_USER");
+        Optional<Role> optionalRole = roleRepository.findRoleByName("ROLE_USER");
         if(!optionalRole.isPresent()){
             Role default_role = new Role();
             default_role.setName("ROLE_USER");
-            this.roleRepository.save(default_role);
+            roleRepository.save(default_role);
             user.setRole(default_role);
         } else {
             user.setRole(optionalRole.get());
         }
-        this.userRepository.save(user);
+        userRepository.save(user);
     }
 
     @Override
     public Optional<Role> findRoleByRoleName(String role_name) {
-        return this.roleRepository.findRoleByName(role_name);
+        return roleRepository.findRoleByName(role_name);
     }
 }
