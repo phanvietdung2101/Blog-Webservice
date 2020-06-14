1bcb57e8448ada0cb78395b5dc00a12a89eaa5ef
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-06-14 21:56:02 +0700

    add content-type to response filter allow-header

--- src/main/java/com/yyy/blog/config/CorsFilter.java
+++ src/main/java/com/yyy/blog/config/CorsFilter.java
@@ -23,7 +23,8 @@ public class CorsFilter implements Filter {
         final HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type");
+//        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         response.setHeader("Access-Control-Max-Age", "3600");
         if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
@@ -39,4 +40,6 @@ public class CorsFilter implements Filter {
     @Override
     public void init(FilterConfig config) throws ServletException {
     }
+
+
 }
